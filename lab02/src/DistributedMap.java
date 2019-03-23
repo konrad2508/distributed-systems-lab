da3b1b010a198bc88ac65eb1fecf6dbb230ae4ba
Konrad <konrad1997@gmail.com>
2019-03-23 01:45:11 +0100

    Change LF to CRLF

--- lab02/src/DistributedMap.java
+++ lab02/src/DistributedMap.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+
+//hi
 public class DistributedMap extends ReceiverAdapter implements SimpleStringMap {
     private JChannel channel;
     private Address ownAddress;
