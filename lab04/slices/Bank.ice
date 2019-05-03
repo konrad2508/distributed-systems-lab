613c80b69b5e503c7a59dd3102713fa91f2d84f8
konrad <konrad1997@gmail.com>
2019-05-04 01:07:29 +0200

    Add currency_table to the bank

--- lab04/slices/Bank.ice
+++ lab04/slices/Bank.ice
@@ -1,5 +1,17 @@
 module Bank{
 
+    enum AccountType { Standard, Premium }
+
+    enum Currency { USD, EUR, GBP, PLN }
+
+    exception AccountException{
+        string message;
+    };
+
+    exception CurrencyException{
+        string message;
+    };
+
     struct ClientData{
         string name;
         string surname;
@@ -7,17 +19,27 @@ module Bank{
         int income;
     };
 
-    interface AccountManagement{
-        Account register(ClientData clientData);
-        Account login();
+    struct AccountData{
+        AccountType accountType;
+        int funds;
     };
 
-    interface Account{
+    struct RegistrationInfo{
+        AccountType accountType;
+        string password;
+    }
 
+    interface Account{
+        AccountData getAccountData();
     };
 
     interface PremiumAccount extends Account{
+        bool getLoan(double amount, Currency currency) throws CurrencyException;
+    };
 
+    interface AccountManagement{
+        string register(ClientData clientData) throws AccountException;
+        Account* login(string id, string password) throws AccountException;
     };
 
 };
--- lab04/src/bank.py
+++ lab04/src/bank.py
@@ -1,33 +1,32 @@
-from __future__ import print_function
-
 import grpc
+from threading import Thread
 
 import currency_pb2
 import currency_pb2_grpc
 
 import sys
 import Ice
-import Demo
+import Bank
+
+currency_table = {}
 
 
-class PrinterI(Demo.Printer):
-    def printString(self, s, current=None):
-        print(s)
-
-
-def run():
+def start_currency_client(requested_currencies):
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = currency_pb2_grpc.CurrencySubscriptionStub(channel)
 
+        currencies_values = map(currency_pb2.Currency.Value, requested_currencies)
+
         prices = stub.Subscribe(
             currency_pb2.SubscribeRequest(
-                currencies=[currency_pb2.Currency.Value('PLN'), currency_pb2.Currency.Value('USD')])
+                currencies=currencies_values)
         )
         while True:
             curr = prices.next()
-            print('currency: %s' % currency_pb2.Currency.Name(curr.currency))
-            print('value: %f' % curr.value)
-            print()
+
+            curr_name = currency_pb2.Currency.Name(curr.currency)
+            curr_value = curr.value
+            currency_table[curr_name] = curr_value
 
 
 def ice_run():
@@ -40,5 +39,6 @@ def ice_run():
 
 
 if __name__ == '__main__':
-    # run()
-    ice_run()
+    currencies = map(str.upper, sys.argv[1:])
+    Thread(target=start_currency_client, args=[currencies]).start()
+    # ice_run()
